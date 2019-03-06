9628ca046027a098e86a6d1c77b51c4e31b9e5de
holeman79 <holeman79@gmail.com>
2019-03-06 22:59:12 +0900

    first commit

---
+++ src/main/java/com/allthatBTS/atbserver/AtbServerApplication.java
@@ -0,0 +1,13 @@
+package com.allthatBTS.atbserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtbServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtbServerApplication.class, args);
+	}
+
+}
