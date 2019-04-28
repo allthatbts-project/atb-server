c35e1b86c2091c20cd83e30840608de5beaf4622
holeman79 <holeman79@gmail.com>
2019-04-29 00:21:21 +0900

    Oauth2 인증 소스 추가

--- src/main/java/com/allthatBTS/atbserver/AtbServerApplication.java
+++ src/main/java/com/allthatBTS/atbserver/AtbServerApplication.java
@@ -1,13 +1,25 @@
 package com.allthatBTS.atbserver;
 
+import com.allthatBTS.atbserver.resolver.UserArgumentResolver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @SpringBootApplication
-public class AtbServerApplication {
+public class AtbServerApplication implements WebMvcConfigurer {
+	@Autowired
+	private UserArgumentResolver userArgumentResolver;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AtbServerApplication.class, args);
 	}
 
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+		argumentResolvers.add(userArgumentResolver);
+	}
 }
---
+++ src/main/java/com/allthatBTS/atbserver/controller/LoginController.java
@@ -0,0 +1,36 @@
+package com.allthatBTS.atbserver.controller;
+
+import com.allthatBTS.atbserver.annotation.SocialUser;
+import com.allthatBTS.atbserver.domain.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/loginSuccess")
+    public String loginComplete(@SocialUser User user, Model model){
+        model.addAttribute("socialInfo", user);
+        return "result";
+    }
+
+//    @GetMapping(value = "/{facebook|google|kakao}/complete")
+//    public String loginComplete(HttpSession httpSession){
+//        OAuth2Authentication authentication = (OAuth2Authentication) SecurityContextHolder.getContext().getAuthentication();
+//        Map<String, String> map = (HashMap<String, String>)authentication.getUserAuthentication().getDetails();
+//        httpSession.setAttribute("user", User.builder()
+//                .name(map.get("name"))
+//                .email(map.get("email"))
+//                .principal(map.get("id"))
+//                .socialType(SocialType.FACEBOOK)
+//                .createdDate(LocalDateTime.now())
+//                .build());
+//        return "redirect:/board/list";
+//    }
+}
